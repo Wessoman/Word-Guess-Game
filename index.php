2bc0a333e202dfce92721210dec914cfcc7c61ea
Antony Hernandez <wessoman@hotmail.com>
2019-05-30 21:52:58 -0500

    KrautRockGame v0.55b

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+    	<meta charset="UTF-8">
+     	
+
+		<!-- Links to reset, bootstrap, font-awesome and my custom css sheets. -->
+		<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		
+		<!-- Title displayed on tab. -->
+		<title>Krautrock Hangman</title>
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<h1>KRAUTROCK HANGMAN</h1>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-6 box">
+					<img class=img-responsive" src="assets/images/game.jpg" width=100%>
+				</div>
+				<div class="col-sm-6 box">
+					<p id="num-wins">Press SPACEBAR to START</p>
+
+					<p>Current band name:</p>
+					<p id="current-word">What Krautrock band can this be??</p>
+
+					<p id="guesses-remaining">15</p>
+					<p>Remaining guesses!</p>
+
+					<P>Letters already guessed:</P>
+					<p id="letters-guessed">None</p>
+				</div>
+			</div>
+		</div>
+
+
+
+	<!-- GAME ENGINE -->
+	<script src="assets/js/game.js" type="text/javascript"></script>
+
+	
+
+
+	</body>
+</html>
